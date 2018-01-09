124b0b4de496b5d7e843d46851b2354905fc94a7
ChrisAngryGodBuckingham <34572466+ChrisAngryGodBuckingham@users.noreply.github.com>
2018-01-09 17:57:16 -0500

    Delete TDA_Base.cpp

--- Source/TDA_Base/Private/Core/TDA_Base.cpp
+++
@@ -1,7 +0,0 @@
-/*CopyRight 2017 Chris "AngryGod" Buckingham M.A.G.Studios Canada*/
-
-#include "TDA_Base.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TDA_Base, "TDA_Base" );
- 
